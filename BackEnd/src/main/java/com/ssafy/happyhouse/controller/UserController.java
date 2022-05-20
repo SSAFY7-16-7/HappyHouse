aef394cad44b39f51ba18680fb5e960e196fa44a
camiyoung <anji6978@naver.com>
2022-05-21 01:11:39 +0900

    user controller  수정

--- BackEnd/src/main/java/com/ssafy/happyhouse/controller/UserController.java
+++ BackEnd/src/main/java/com/ssafy/happyhouse/controller/UserController.java
@@ -147,7 +147,7 @@ public class UserController {
 
 	// 회원 삭제 delete /user
 	@ApiOperation(value="회원 삭제", notes = "id가 일치하는 회원 데이터 1개 삭제")
-	@PostMapping("/remove")
+	@DeleteMapping("/remove")
 	public ResponseEntity<?> userRemove(@RequestBody String id,HttpServletRequest request) {
 		try {
 			System.out.println(id);
