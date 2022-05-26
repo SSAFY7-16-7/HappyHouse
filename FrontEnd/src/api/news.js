ac835cd6a2cfe6d3ec25709ef779fa175e160869
camiyoung <anji6978@naver.com>
2022-05-26 14:32:57 +0900

    부동산 뉴스 변경

--- FrontEnd/src/api/news.js
+++ FrontEnd/src/api/news.js
@@ -1,16 +1,16 @@
 const news = {
   search_metadata: {
-    id: "628e4d1bd5a5312a8f382cbd",
+    id: "628f10847bea270375eee031",
     status: "Success",
     json_endpoint:
-      "https://serpapi.com/searches/e3931613838deb21/628e4d1bd5a5312a8f382cbd.json",
-    created_at: "2022-05-25 15:36:59 UTC",
-    processed_at: "2022-05-25 15:36:59 UTC",
+      "https://serpapi.com/searches/e3931613838deb21/628f10847bea270375eee031.json",
+    created_at: "2022-05-26 05:30:44 UTC",
+    processed_at: "2022-05-26 05:30:44 UTC",
     naver_url:
       "https://search.naver.com/search.naver?query=%EB%B6%80%EB%8F%99%EC%82%B0&where=news",
     raw_html_file:
-      "https://serpapi.com/searches/e3931613838deb21/628e4d1bd5a5312a8f382cbd.html",
-    total_time_taken: 1.59,
+      "https://serpapi.com/searches/e3931613838deb21/628f10847bea270375eee031.html",
+    total_time_taken: 1.46,
   },
   search_parameters: {
     query: "부동산",
@@ -20,123 +20,150 @@ const news = {
   news_results: [
     {
       position: 1,
+      title:
+        '尹대통령 "부동산 세 부담, 한시적 완화"…文정부 부동산 정책 되돌리기',
+      link: "https://www.pressian.com/pages/articles/2022052612062080924?utm_source=naver&utm_medium=search",
+      thumbnail:
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F002%2F2022%2F05%2F26%2F2245083.jpg&type=ofullfill264_180_gray&expire=2&refresh=true",
+      news_info: {
+        press_name: "프레시안언론사 선정",
+        news_date: "2시간 전",
+      },
+      snippet:
+        '윤 대통령은 모두발언에서 "부동산 시장의 안정과 국민의 주거 복지 향상을 위해서 한시적으로 세 부담을... 세 부담을 줄여 시장에 나오는 매물을 늘리기 위한 조치로, 사실상 문재인 정부가 추진한 부동산 세제 정책이...',
+    },
+    {
+      position: 2,
+      title: "1분기 전국 상업·업무용부동산 ㎡당 매매가 606만원",
+      link: "https://www.yna.co.kr/view/AKR20220526072800003?input=1195m",
+      thumbnail:
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F001%2F2022%2F05%2F26%2F13206136.jpg&type=ff264_180&expire=2&refresh=true",
+      news_info: {
+        press_name: "연합뉴스",
+        news_date: "2시간 전",
+      },
+      snippet:
+        "부동산R114 분석…1분기 기준으로 올해가 역대 최고 사회적 거리두기 완화에 따른 상권 회복세가 나타나는 가운데 상업·업무용 부동산의 매매가도 오르는 분위기다. 26일 부동산R114가 자사 상업용 부동산 분석...",
+    },
+    {
+      position: 3,
+      title: "1분기 창업기업 약 35만개로 3.4% 감소‥부동산업 15.6%↓",
+      link: "https://imnews.imbc.com/news/2022/econo/article/6372422_35687.html",
+      thumbnail:
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F214%2F2022%2F05%2F26%2F1198924.jpg&type=ff264_180&expire=2&refresh=true",
+      news_info: {
+        press_name: "MBC",
+        news_date: "4시간 전",
+      },
+      snippet:
+        "부동산업과 숙박·음식점업 창업은 전년 동기대비 각각 15.6%, 5.4% 감소했는데, 부동산업은 2020년 사업자가 급증한 데 따른 기저효과로 창업이 줄어든 것으로 분석됩니다. 기술기반업종 중에서는 코로나19 유행에...",
+    },
+    {
+      position: 4,
+      title: "정의로운 부동산 세금의 기준",
+      link: "https://news.kbs.co.kr/news/view.do?ncd=5471485&ref=A",
+      thumbnail:
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F056%2F2022%2F05%2F26%2F11272772.jpg&type=ff264_180&expire=2&refresh=true",
+      news_info: {
+        press_name: "KBS언론사 선정",
+        news_date: "7시간 전",
+      },
+      snippet:
+        "국책연구기관인 대외경제정책연구원 KIEP가 부동산 세금의 기준을 내놨습니다. 보유세는 올려라, 거래세는 낮춰라. 불평등도 염두에 두고 내린 결론이라고 합니다. <정의로운> 부동산 세금의 기준이라고 이름 붙여...",
+    },
+    {
+      position: 5,
       title: '대외硏 "韓, 부동산 안정 위해 보유세 높이고 거래세 낮춰야"',
       link: "http://www.fnnews.com/news/202205251612501452",
       thumbnail:
         "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F014%2F2022%2F05%2F25%2F4841989.jpg&type=ff264_180&expire=2&refresh=true",
       news_info: {
         press_name: "파이낸셜뉴스언론사 선정",
-        news_date: "8시간 전",
+        news_date: "22시간 전",
       },
       snippet:
         "국책연구기관인 대외경제정책연구원(KIEP)이 한국의 부동산 시장 안정을 위해 보유세를 점차 높이고 거래세는 낮추는 정책 조합이 필요하다는 제언을 내놨다. KIEP는 25일 '국제사회의 부동산 보유세 논의 방향과...",
     },
     {
-      position: 2,
-      title: "부동산 침체, 고령화에 인구이동 48년만에 최저…탈서울 계속",
-      link: "https://www.joongang.co.kr/article/25074044",
+      position: 6,
+      title: "'사면초가' 가구업계...부동산 정책 탈출구될까?",
+      link: "http://www.edaily.co.kr/news/newspath.asp?newsid=02801126632332856",
       thumbnail:
-        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F025%2F2022%2F05%2F25%2F3197522.jpg&type=ff264_180&expire=2&refresh=true",
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F018%2F2022%2F05%2F26%2F5226000.jpg&type=ff264_180&expire=2&refresh=true",
       news_info: {
-        press_name: "중앙일보",
-        news_date: "12시간 전",
+        press_name: "이데일리",
+        news_date: "39분 전",
       },
       snippet:
-        "부동산 시장이 가라앉은 데다, 이사를 꺼리는 고령 인구 비중이 크게 늘어나면서다. 25일 통계청이 발간한... 금리 상승, 대출 규제 등으로 부동산 시장이 침체한 영향이 컸다. 실제 국토교통부 통계를 보면 지난달...",
+        "정부의 부동산 규제 완화가 업황 개선에 도움이 될지 이지은 기자가 취재했습니다. 국내 대표 가구업체인... 가구업계는 부동산 정책 변화에 기대를 걸고 있습니다. 규제 완화 및 불확실성 해소로 주택매매 거래가 확대될...",
     },
     {
-      position: 3,
-      title: "롯데건설, 부동산개발 투자펀드 협약…디벨로퍼 사업 속도",
-      link: "http://www.newsis.com/view/?id=NISX20220525_0001885142&cID=10401&pID=10400",
+      position: 7,
+      title: "집 팔아야하는데…금리 인상에 부동산 거래 더 얼어붙을까",
+      link: "https://view.asiae.co.kr/article/2022052608432104793",
       thumbnail:
-        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F003%2F2022%2F05%2F25%2F11208477.jpg&type=ff264_180&expire=2&refresh=true",
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F277%2F2022%2F05%2F26%2F5094769.jpg&type=ff264_180&expire=2&refresh=true",
       news_info: {
-        press_name: "뉴시스",
-        news_date: "9시간 전",
+        press_name: "아시아경제",
+        news_date: "18분 전",
       },
       snippet:
-        "롯데건설이 부동산개발 투자펀드 조성을 위해 케이클라비스자산운용과 업무협약(MOU)을 맺었다. 롯데건설은 전날 롯데건설 본사 대회의실에서 케이클라비스자산운용과 '부동산개발 투자펀드를 위한 MOU'를 체결했다고...",
+        "정부 부동산 정책 변화 등의 변수가 맞물리면서 구매 심리 위축이 예상되기 때문이다. 매물 증가와 거래 절벽이 동시에 지속되는 현상이 당분간 계속되면서 집값도 횡보할 것으로 예상된다. 26일 부동산 빅데이터업체...",
     },
-    {
-      position: 4,
-      title: "中 부동산침체 장기화에 미분양 물량 4년 만에 최다",
-      link: "https://biz.chosun.com/international/international_general/2022/05/25/WJRIA3U6UJBZLAXQZSVJSJ6UZE/?utm_source=naver&utm_medium=original&utm_campaign=biz",
-      thumbnail:
-        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F366%2F2022%2F05%2F25%2F816408.jpg&type=ff264_180&expire=2&refresh=true",
-      news_info: {
-        press_name: "조선비즈",
-        news_date: "8시간 전",
-      },
-      snippet:
-        "부동산 시장 침체가 지속되면서 중국의 미분양 부동산 물량이 4년 만에 최고치를 기록했다. 25일 중국 통계국 자료에 따르면 지난달 말 기준 전국 아파트와 상가, 오피스텔 등 부동산의 미분양 면적은 5억5천700만...",
-    },
-    {
-      position: 5,
-      title: '롯데건설, 부동산개발 투자펀드 협약…"디벨로퍼 사업 가속화"',
-      link: "https://www.news1.kr/articles/?4691052",
-      thumbnail:
-        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F421%2F2022%2F05%2F25%2F6115233.jpg&type=ff264_180&expire=2&refresh=true",
-      news_info: {
-        press_name: "뉴스1",
-        news_date: "15시간 전",
-      },
-      snippet:
-        "롯데건설은 롯데건설 본사 대회의실에서 케이클라비스자산운용과 '부동산개발 투자펀드를 위한 업무협약... 등 부동산 개발사업 전 단계에 참여하는 기업이다. 이번 협약은 롯데건설이 종합 디벨로퍼로서 사업을 확장하기...",
-    },
-
     {
       position: 8,
-      title: '한국부동산원 "지난해 전국 아파트 월평균 관리비 18만원"',
-      link: "https://www.sedaily.com/NewsView/2663UCJZRR",
+      title: "부동산정책 우선순위, 집값안정>투기근절>청년·신혼지원 확대 [KSOI]",
+      link: "http://news.heraldcorp.com/view.php?ud=20220525000967",
       thumbnail:
-        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F011%2F2022%2F05%2F25%2F4057874.jpg&type=ff264_180&expire=2&refresh=true",
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F016%2F2022%2F05%2F26%2F1995803.jpg&type=ofullfill264_180_gray&expire=2&refresh=true",
       news_info: {
-        press_name: "서울경제",
-        news_date: "13시간 전",
+        press_name: "헤럴드경제",
+        news_date: "3시간 전",
       },
       snippet:
-        "25일 한국부동산원이 공동주택관리정보시스템(K-apt) 현황을 분석한 결과 지난해... 한국부동산원은 올해 관리비 규모는 연간 24조원 규모에 달할 것으로 추정했다. 관리비공개의무단지는 2017년 904만...",
+        "바라는 부동산정책은 ‘집값 안정’인 것으로 나타났다. 헤럴드경제가 여론조사 전문기관 한국사회여론연구소(KSOI)에 의뢰해 지난 23~24일 이틀간 서울시에 거주하는 18세 이상 남녀 805명을 대상으로 ‘서울시 부동산...",
     },
     {
       position: 9,
-      title: "은마아파트 30평대 실거래가 1억 원↑…서울 부동산 다시 들썩?",
-      link: "http://www.munhwa.com/news/view.html?no=20220525MW095939774215",
+      title:
+        "서울 빌라 전·월세 거래 '역대급'…아파트 매매 10건 중 6건 '상승' [식후땡...",
+      link: "https://www.hankyung.com/realestate/article/2022052630076",
       thumbnail:
-        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F021%2F2022%2F05%2F25%2F2514945.jpg&type=ff264_180&expire=2&refresh=true",
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F015%2F2022%2F05%2F26%2F4703990.jpg&type=ff264_180&expire=2&refresh=true",
       news_info: {
-        press_name: "문화일보언론사 선정",
-        news_date: "14시간 전",
+        press_name: "한국경제언론사 선정",
+        news_date: "1시간 전",
       },
       snippet:
-        "윤석열 정부 출범 후 재개발·재건축 규제 완화에 대한 기대감이 시장에 반영되면서, 재차 부동산 시장이... 김 의원은 “부동산 시장 불안 심리가 재확산할 수 있다는 우려가 크다”며 “부동산 시장은 심리요인...",
+        "부동산 시장이 다시 들썩일 수 있을 것이란 전망입니다. 오늘도 부동산 관련 뉴스 전해드립니다. ◆아파트 전세 너무 비싸, 서울 빌라 전·월세 거래량 ‘역대급’ 첫 번째 뉴스입니다. 서울부동산정보광장에 따르면...",
     },
     {
       position: 10,
-      title: '[마켓인]"부동산 단점을 장점으로 만든 리츠…안정적 설계 중요"',
-      link: "http://www.edaily.co.kr/news/newspath.asp?newsid=01111926632332528",
+      title: '"부동산 해법 바뀐 민주당, 대선 이후 두달 새 무슨 일 있었나"',
+      link: "https://www.pressian.com/pages/articles/2022052611465550710?utm_source=naver&utm_medium=search",
       thumbnail:
-        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F018%2F2022%2F05%2F25%2F5224686.jpg&type=ff264_180&expire=2&refresh=true",
+        "https://search.pstatic.net/common/?src=https%3A%2F%2Fimgnews.pstatic.net%2Fimage%2Forigin%2F002%2F2022%2F05%2F26%2F2245095.jpg&type=ff264_180&expire=2&refresh=true",
       news_info: {
-        press_name: "이데일리",
-        news_date: "20시간 전",
+        press_name: "프레시안언론사 선정",
+        news_date: "19분 전",
       },
       snippet:
-        "(REITs·부동산투자신탁)까지도 공모청약 실시간 경쟁률을 전해주는 시대가 됐다며 리츠에 대한 일반... 소문난 부동산 전문가다. 그는 “경쟁사에서도 리츠 투자와 관련해 꾸준히 상담이나 자문 요청이 들어온다”며...",
+        "최경호 주거중립성연구소 수처작주 소장 '종합부동산세 완화, 다주택자 양도세 유예, 재건축 안전진단 폐지... 부동산 공약이 모두 규제 완화에 역점을 두고 있다. 그간 민주당에서 해왔던 정책들과는 정반대다. 이는 송...",
     },
   ],
   pagination: {
     current: 1,
-    next: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=11",
+    next: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=20&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=11",
     other_pages: {
-      2: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=11",
-      3: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=21",
-      4: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=31",
-      5: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=41",
-      6: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=51",
-      7: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=61",
-      8: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=71",
-      9: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=81",
-      10: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=31&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=91",
+      2: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=20&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=11",
+      3: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=20&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=21",
+      4: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=20&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=31",
+      5: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=20&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=41",
+      6: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=20&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=51",
+      7: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=20&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=61",
+      8: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=20&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=71",
+      9: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=20&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=81",
+      10: "https://search.naver.com?where=news&sm=tab_pge&query=%EB%B6%80%EB%8F%99%EC%82%B0&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=20&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start=91",
     },
   },
   serpapi_pagination: {
